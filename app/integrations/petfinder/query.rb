9f88cb3bd8ea44c8c7705d16c4bbc5bb0d9ae8b1
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2014-08-16 22:49:42 -0400

    Implemented inclusive query filter and added PetsThoughShelter query * Added PetfinderIntegration::Query::PetsThroughShelter.get_with_filter_inclusive * Added PetfinderIntegration::Query.inclusive_filter * Tested and implemented. Inclusive_filter could use its own though

--- app/integrations/petfinder/query.rb
+++ app/integrations/petfinder/query.rb
@@ -7,8 +7,20 @@ module PetfinderIntegration
         PetfinderIntegration::Models::Shelter.new(result['petfinder']['shelter']).run
       end
     end
-    module PetThroughShelter
-        # params = params.marge(options)
+    module PetsThroughShelter
+      def self.get(params)
+        result = PetfinderIntegration::Connection.get('shelter.getPets', params)
+        (result['petfinder']['pets']['pet'] || []).map do |pet|
+          PetfinderIntegration::Models::Pet.new(pet).run
+        end
+      end
+
+      def self.get_with_filter_inclusive(params, filters)
+        result = self.get(params)
+        filters.map do |filter|
+          result.select { |pet| (pet.send(filter.keys[0].to_sym).downcase).include? filter.values[0].downcase }
+        end.flatten.uniq
+      end
     end
   end
 end
--- spec/integrations/petfinder/query_spec.rb
+++ spec/integrations/petfinder/query_spec.rb
@@ -20,6 +20,35 @@ describe PetfinderIntegration::Query::Shelter, :focus => true do
   end
 end
 
-describe PetfinderIntegration::Query::PetThroughShelter, :focus => true do
-  subject { PetfinderIntegration::Query::PetThroughShelter }
+describe PetfinderIntegration::Query::PetsThroughShelter, :focus => true do
+  subject { PetfinderIntegration::Query::PetsThroughShelter }
+
+  let(:shelter_get_10_pets_from_NY803) {
+    VCR.use_cassette 'shelter_getpets/10_from_NY803' do
+      Curl::Easy.perform('http://api.petfinder.com/shelter.getPets?key=3ea4a92e053a83bda1d17959baf8b158&id=NY803&format=json&output=full&count=10')
+    end
+  }
+  let(:shelter_get_10_pets_from_NY803_json) { JSON.parse(shelter_get_10_pets_from_NY803.body) }
+
+  describe '.get' do
+    it 'should retrive pets information by shelter id NY803' do
+      params = { 'output' => 'full', 'count' => '10', 'id' => 'NY803' }
+      expect(PetfinderIntegration::Connection).to receive(:get).with("shelter.getPets", params).and_return(shelter_get_10_pets_from_NY803_json)
+
+      result = PetfinderIntegration::Query::PetsThroughShelter.get(params)
+      expect(result.first).to be_instance_of PetfinderIntegration::Models::Pet
+      expect(result.count).to eq 10
+    end
+  end
+  describe '.get_with_filter_inclusive' do
+    it 'should retrive pets by shelter id NY803 and filter only dogs and cats' do
+      params = { 'output' => 'full', 'count' => '10', 'id' => 'NY803' }
+      filters = [{ 'animal' => 'cat' }, { 'animal' => 'dog' }]
+      expect(PetfinderIntegration::Connection).to receive(:get).with("shelter.getPets", params).and_return(shelter_get_10_pets_from_NY803_json)
+      result = PetfinderIntegration::Query::PetsThroughShelter.get_with_filter_inclusive(params, filters)
+      expect(result.count).to eq 7
+      animals = result.map { |pet| pet.animal }
+      expect(animals - ["Cat", "Dog"]).to eq []
+    end
+  end
 end
